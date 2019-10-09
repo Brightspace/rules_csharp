5822781d13fcc7bc81c751744519208d7a525fe6
jrbeverly <Jonathan.Beverly@D2L.com>
2019-10-09 13:19:56 -0400

    Example usage of csharp_resx rule

---
+++ examples/resgen/hello.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Threading;
+using System.Globalization;
+namespace Hello {
+  public static class Program {
+    public static void Main() {
+      Console.WriteLine("Message: {0}", Strings.Hello);
+    }
+  }
+}
