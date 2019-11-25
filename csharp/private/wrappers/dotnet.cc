56e04849fd7b607589c12295a8338daab6ba8b9a
jrbeverly <Jonathan.Beverly@D2L.com>
2019-11-25 10:08:52 -0500

    Run format on wrapper

--- csharp/private/wrappers/dotnet.cc
+++ csharp/private/wrappers/dotnet.cc
@@ -1,42 +1,38 @@
 #include <iostream>
-#include <string>
 #include <sstream>
+#include <string>
 
 #ifdef _WIN32
-#include <windows.h>
-#include <process.h>
 #include <errno.h>
-#else // not _WIN32
+#include <process.h>
+#include <windows.h>
+#else  // not _WIN32
 #include <stdlib.h>
 #include <unistd.h>
-#endif // _WIN32
+#endif  // _WIN32
 
 #include "tools/cpp/runfiles/runfiles.h"
 
 using bazel::tools::cpp::runfiles::Runfiles;
 
-std::string evprintf(std::string name, std::string path)
-{
+std::string evprintf(std::string name, std::string path) {
   std::stringstream ss;
   ss << name << "=" << path;
   return ss.str();
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char** argv) {
   std::string error;
 
   auto runfiles = Runfiles::Create(argv[0], &error);
 
-  if (runfiles == nullptr)
-  {
+  if (runfiles == nullptr) {
     std::cerr << "Couldn't load runfiles: " << error << std::endl;
     return 101;
   }
 
   auto dotnet = runfiles->Rlocation("{DotnetExe}");
-  if (dotnet.empty())
-  {
+  if (dotnet.empty()) {
     std::cerr << "Couldn't find the .NET runtime" << std::endl;
     return 404;
   }
@@ -57,14 +53,14 @@ int main(int argc, char **argv)
   envvars.push_back(evprintf("APPDATA", dotnetDir));
   envvars.push_back(evprintf("PROGRAMFILES", dotnetDir));
   envvars.push_back(evprintf("USERPROFILE", dotnetDir));
-  envvars.push_back(evprintf("DOTNET_CLI_TELEMETRY_OPTOUT", "1")); // disable telemetry
+  envvars.push_back(
+      evprintf("DOTNET_CLI_TELEMETRY_OPTOUT", "1"));  // disable telemetry
 
   // dotnet wants this to either be dotnet or dotnet.exe but doesn't have a
   // preference otherwise.
-  auto dotnet_argv = new char *[argc];
-  dotnet_argv[0] = (char *)"dotnet";
-  for (int i = 1; i < argc; i++)
-  {
+  auto dotnet_argv = new char*[argc];
+  dotnet_argv[0] = (char*)"dotnet";
+  for (int i = 1; i < argc; i++) {
     dotnet_argv[i] = argv[i];
   }
   dotnet_argv[argc] = nullptr;
@@ -73,8 +69,7 @@ int main(int argc, char **argv)
   // _spawnve has a limit on the size of the environment variables
   // passed to the process. So here we will set the environment
   // variables for this process, and the spawned instance will inherit them
-  for (int i = 1; i < envvars.size(); i++)
-  {
+  for (int i = 1; i < envvars.size(); i++) {
     putenv(envvars[i].c_str());
   }
 
@@ -83,9 +78,8 @@ int main(int argc, char **argv)
   auto result = _spawnv(_P_WAIT, dotnet.c_str(), dotnet_argv);
 #else
   auto envc = envvars.size();
-  auto envp = new char*[envc+1];
-  for (uint i = 0; i < envc; i++)
-  {
+  auto envp = new char*[envc + 1];
+  for (uint i = 0; i < envc; i++) {
     envp[i] = &envvars[i][0];
   }
   envp[envc] = nullptr;
@@ -93,9 +87,8 @@ int main(int argc, char **argv)
   // run `dotnet.exe` and wait for it to complete
   // the output from this cmd will be emitted to stdout
   auto result = execve(dotnet.c_str(), const_cast<char**>(dotnet_argv), envp);
-#endif // _WIN32
-  if (result != 0)
-  {
+#endif  // _WIN32
+  if (result != 0) {
     std::cout << "dotnet failed: " << errno << std::endl;
     return -1;
   }
